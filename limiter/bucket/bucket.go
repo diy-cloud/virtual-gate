8d9a13db7a1c680cb5f860c24eca2a836ab25fb6
snowmerak <snowmerak@outlook.com>
2022-03-06 06:53:18 +0900

    remove remote ip limit

--- limiter/bucket/bucket.go
+++ limiter/bucket/bucket.go
@@ -1,67 +1,35 @@
 package bucket
 
 import (
-	"encoding/hex"
 	"net/http"
-	"sync"
 	"time"
 
 	"github.com/diy-cloud/virtual-gate/limiter"
 	"github.com/diy-cloud/virtual-gate/lock"
 )
 
-var nodePool = sync.Pool{
-	New: func() interface{} {
-		return new(node)
-	},
-}
-
-type node struct {
-	value string
-	next  *node
-}
-
 type Bucket struct {
-	recentlyTakensHead    *node
-	recentlyTakensTail    *node
-	recentlyTakensSet     map[string]int64
-	lock                  *lock.Lock
-	maxTokens             int64
-	tokens                int64
-	regenTime             time.Duration
-	sameRemoteIPLimitRate float64
-	ch                    chan struct{}
+	lock      *lock.Lock
+	maxTokens int64
+	tokens    int64
+	regenTime time.Duration
+	ch        chan struct{}
 }
 
-func New(maxTokens, regenPerSecond int, sameRemoteIPLimitRate float64) limiter.Limiter {
+func New(maxTokens, regenPerSecond int) limiter.Limiter {
 	ch := make(chan struct{}, maxTokens)
 	bucket := &Bucket{
-		recentlyTakensHead:    nil,
-		recentlyTakensTail:    nil,
-		recentlyTakensSet:     make(map[string]int64),
-		lock:                  new(lock.Lock),
-		maxTokens:             int64(maxTokens),
-		tokens:                int64(maxTokens),
-		regenTime:             time.Second / time.Duration(regenPerSecond),
-		sameRemoteIPLimitRate: sameRemoteIPLimitRate,
-		ch:                    ch,
+		lock:      new(lock.Lock),
+		maxTokens: int64(maxTokens),
+		tokens:    int64(maxTokens),
+		regenTime: time.Second / time.Duration(regenPerSecond),
+		ch:        ch,
 	}
 	go func() {
 		for range ch {
 			time.Sleep(bucket.regenTime)
 			bucket.lock.Lock()
 			bucket.tokens++
-			if bucket.recentlyTakensHead == nil {
-				bucket.lock.Unlock()
-				continue
-			}
-			node := bucket.recentlyTakensHead
-			bucket.recentlyTakensHead = node.next
-			if bucket.recentlyTakensHead == nil {
-				bucket.recentlyTakensTail = nil
-			}
-			bucket.recentlyTakensSet[node.value]--
-			nodePool.Put(node)
 			bucket.lock.Unlock()
 		}
 	}()
@@ -79,41 +47,8 @@ func (b *Bucket) decreaseToken() (bool, int) {
 	return true, http.StatusOK
 }
 
-func (b *Bucket) checkTaken(key string) (bool, int) {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-	if b.recentlyTakensSet[key] >= int64(float64(b.maxTokens)*b.sameRemoteIPLimitRate) {
-		return false, http.StatusTooManyRequests
-	}
-	return true, http.StatusOK
-}
-
-func (b *Bucket) appendTaken(key string) (bool, int) {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-	b.recentlyTakensSet[key]++
-	newNode := nodePool.Get().(*node)
-	newNode.value = key
-	newNode.next = nil
-	if b.recentlyTakensHead == nil {
-		b.recentlyTakensHead = newNode
-		b.recentlyTakensTail = newNode
-		return true, http.StatusOK
-	}
-	b.recentlyTakensTail.next = newNode
-	b.recentlyTakensTail = newNode
-	return true, http.StatusOK
-}
-
-func (b *Bucket) TryTake(key []byte) (bool, int) {
-	hexKey := hex.EncodeToString(key)
-	if passed, code := b.checkTaken(hexKey); !passed {
-		return passed, code
-	}
+func (b *Bucket) TryTake(_ []byte) (bool, int) {
 	if passed, code := b.decreaseToken(); !passed {
-		return passed, code
-	}
-	if passed, code := b.appendTaken(hexKey); !passed {
 		return passed, code
 	}
 	return true, http.StatusOK
