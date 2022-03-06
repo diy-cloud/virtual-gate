199206bc3d869046811e496172b11a07051c08f2
snowmerak <snowmerak@outlook.com>
2022-03-06 19:23:57 +0900

    add http proxy

---
+++ proxy/http_proxy/http.go
@@ -0,0 +1,75 @@
+package http_proxy
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/diy-cloud/virtual-gate/lock"
+)
+
+type handler struct {
+	h func(w http.ResponseWriter, r *http.Request) bool
+	l *lock.Lock
+}
+
+type proxyMap struct {
+	m map[string]*httputil.ReverseProxy
+	l *lock.Lock
+}
+
+type HttpProxy struct {
+	handler  handler
+	proxyMap proxyMap
+}
+
+func New() *HttpProxy {
+	hp := new(HttpProxy)
+
+	hp.handler.h = func(w http.ResponseWriter, r *http.Request) bool { return true }
+	hp.handler.l = new(lock.Lock)
+
+	hp.proxyMap.m = make(map[string]*httputil.ReverseProxy)
+	hp.proxyMap.l = new(lock.Lock)
+
+	return hp
+}
+
+func (hp *HttpProxy) SetHandler(h func(w http.ResponseWriter, r *http.Request) bool) {
+	hp.handler.l.Lock()
+	defer hp.handler.l.Unlock()
+	hp.handler.h = h
+}
+
+func (hp *HttpProxy) SetUpstreamServer(name string, rawURL string) error {
+	url, err := url.Parse(rawURL)
+	if err != nil {
+		return fmt.Errorf("HttpProxy.SetUpstreamServer: url.Parse: %w", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	hp.proxyMap.l.Lock()
+	defer hp.proxyMap.l.Unlock()
+	if _, ok := hp.proxyMap.m[name]; !ok {
+		delete(hp.proxyMap.m, name)
+	}
+	hp.proxyMap.m[name] = proxy
+	return nil
+}
+
+func (hp *HttpProxy) ServeHTTP(name string, w http.ResponseWriter, r *http.Request) {
+	hp.handler.l.Lock()
+	defer hp.handler.l.Unlock()
+	if !hp.handler.h(w, r) {
+		return
+	}
+
+	hp.proxyMap.l.Lock()
+	defer hp.proxyMap.l.Unlock()
+	if proxy, ok := hp.proxyMap.m[name]; ok {
+		proxy.ServeHTTP(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
