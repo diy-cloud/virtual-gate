2529715d6124e0320df018d2e8361a5c4dc58291
snowmerak <snowmerak@outlook.com>
2022-03-09 18:34:30 +0900

    add not found error

--- circuit_breaker/breaker.go
+++ circuit_breaker/breaker.go
@@ -1,4 +1,15 @@
 package circuit_breaker
 
+import "errors"
+
 type CurciutBreaker interface {
+	BreakDown(target string) error
+	Restore(target string) error
+	IsBrokeDown(target string) bool
+}
+
+var notFoundErr = errors.New("not found")
+
+func ErrorNotFound() error {
+	return notFoundErr
 }
