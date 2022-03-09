b937905f68e0df65fa1e923070b09d3ef3467373
snowmerak <snowmerak@outlook.com>
2022-03-09 18:34:36 +0900

    create simple breaker

---
+++ circuit_breaker/breaker/breaker.go
@@ -0,0 +1,39 @@
+package breaker
+
+import (
+	"github.com/diy-cloud/virtual-gate/circuit_breaker"
+	"github.com/diy-cloud/virtual-gate/lock"
+)
+
+type Breaker struct {
+	cache map[string]struct{}
+	l     *lock.Lock
+}
+
+func New() circuit_breaker.CurciutBreaker {
+	return &Breaker{
+		cache: make(map[string]struct{}),
+		l:     new(lock.Lock),
+	}
+}
+
+func (b *Breaker) BreakDown(target string) error {
+	b.l.Lock()
+	defer b.l.Unlock()
+	b.cache[target] = struct{}{}
+	return nil
+}
+
+func (b *Breaker) Restore(target string) error {
+	b.l.Lock()
+	defer b.l.Unlock()
+	delete(b.cache, target)
+	return nil
+}
+
+func (b *Breaker) IsBrokeDown(target string) bool {
+	b.l.Lock()
+	defer b.l.Unlock()
+	_, ok := b.cache[target]
+	return ok
+}
