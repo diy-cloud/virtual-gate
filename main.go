586cc483780caf9eeae2ae92b420e2a3aaede849
snowmerak <snowmerak@outlook.com>
2022-03-09 13:31:06 +0900

    update test code

--- main.go
+++ main.go
@@ -1,32 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net"
+	"sync"
+	"time"
 
-	"github.com/diy-cloud/virtual-gate/proxy/tcp_proxy"
+	"github.com/diy-cloud/virtual-gate/balancer/round"
+	"github.com/diy-cloud/virtual-gate/lock"
 )
 
 func main() {
-	tcpProxy := tcp_proxy.NewTcpProxy("localhost:8080")
+	balancer := round.New()
 
-	lis, err := net.ListenTCP("tcp", &net.TCPAddr{
-		IP:   net.ParseIP("0.0.0.0"),
-		Port: 8000,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	ids := []string{"a", "b", "c", "d", "e", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 
-	for {
-		conn, err := lis.AcceptTCP()
-		if err != nil {
-			log.Fatal(err)
-		}
-		go func() {
-			if err := tcpProxy.Connect(conn); err != nil {
+	balancer.Add("a")
+	balancer.Add("b")
+	balancer.Add("c")
+	balancer.Add("d")
+	balancer.Add("e")
+
+	used := map[string]int{}
+	lock := new(lock.Lock)
+
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(id string) {
+			defer wg.Done()
+			target, err := balancer.Get(id)
+			if err != nil {
 				log.Println(err)
+				return
 			}
-		}()
+			lock.Lock()
+			used[target]++
+			lock.Unlock()
+			time.Sleep(time.Millisecond * 100)
+			balancer.Restore(target)
+		}(ids[i%len(ids)])
 	}
+	wg.Wait()
+
+	fmt.Println(used)
 }
