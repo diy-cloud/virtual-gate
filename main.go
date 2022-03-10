ec71f79f96e6122b4fcfbe5943ff4e432fc0a4ff
snowmerak <snowmerak@outlook.com>
2022-03-10 17:06:48 +0900

    update test code

--- main.go
+++ main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/diy-cloud/virtual-gate/breaker/count_breaker"
 )
 
 func main() {
-	breaker := count_breaker.New(10)
-	brokenCount := 0
-	for i := 0; i < 100; i++ {
-		if breaker.IsBrokeDown("test") {
-			brokenCount++
-			i--
+	l := make([]int, 10000)
+	for k := 0; k < 10000; k++ {
+		breaker := count_breaker.New(100)
+		brokenCount := 0
+		for i := 0; i < 10000; i++ {
+			if breaker.IsBrokeDown("test") {
+				brokenCount++
+				i--
+				continue
+			}
+			if rand.Int63n(100) < 30 {
+				breaker.BreakDown("test")
+				continue
+			}
+			breaker.Restore("test")
 		}
-		if i%3 == 0 {
-			breaker.BreakDown("test")
-			continue
-		}
-		breaker.Restore("test")
+		l[k] = brokenCount
 	}
-	fmt.Println(brokenCount)
+	avg := 0
+	for _, v := range l {
+		avg += v
+	}
+	avg = avg / len(l)
+	fmt.Println(avg)
 }
