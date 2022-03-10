9cfc4b8f72d54c6718ed694355551ba4281f8b41
snowmerak <snowmerak@outlook.com>
2022-03-11 04:42:35 +0900

    update test code

--- main.go
+++ main.go
@@ -13,11 +13,21 @@ import (
 func main() {
 	limiter := slide_count.New(30000, time.Microsecond)
 	acl := acl.New(60, time.Microsecond)
-	breaker := count_breaker.New(8, 10)
+	breaker := count_breaker.New(200, 10)
 	balancer := least.New()
+	balancer.Add("localhost:8080")
+	balancer.Add("localhost:8081")
+	balancer.Add("localhost:8082")
+	balancer.Add("localhost:8083")
+	balancer.Add("localhost:8084")
+	balancer.Add("localhost:8085")
+	balancer.Add("localhost:8086")
+	balancer.Add("localhost:8087")
+	balancer.Add("localhost:8088")
+	balancer.Add("localhost:8089")
 	proxy := http_proxy.NewHttp()
 
-	if err := proxy.Serve("0.0.0.0:80", limiter, acl, breaker, balancer); err != nil {
+	if err := proxy.Serve("0.0.0.0:9999", limiter, acl, breaker, balancer); err != nil {
 		panic(err)
 	}
 }
